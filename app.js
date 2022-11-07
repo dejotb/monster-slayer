3fcf14908c4de290ed33035113db6cf138526d52
dejotb <dejotb@gmail.com>
2022-11-07 19:12:30 +0100

    added heal and special attack functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getAttackValue(max, min) {
+function getRandomValue(max, min) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      roundNumber: 0,
     };
   },
 
@@ -17,21 +18,43 @@ const app = Vue.createApp({
     monsterBarStyles() {
       return { width: `${this.monsterHealth}%` };
     },
+
+    activeSpecialAttack() {
+      return this.roundNumber % 3 !== 0;
+    },
+
+    // ableToHeal() {
+    //   return this.playerHealth >= 80;
+    // },
   },
 
   methods: {
     attackMonster() {
-      this.monsterHealth -= getAttackValue(12, 5);
+      this.roundNumber++;
+      this.monsterHealth -= getRandomValue(5, 12);
       setTimeout(this.attackPlayer, 500);
+      console.log(this.roundNumber);
     },
 
     attackPlayer() {
-      this.playerHealth -= getAttackValue(15, 8);
+      this.playerHealth -= getRandomValue(8, 15);
     },
 
     specialAttack() {
-      this.monsterHealth -= getAttackValue(30, 10);
-      setTimeout(this.attackPlayer, 1000);
+      this.roundNumber++;
+      this.monsterHealth -= getRandomValue(10, 30);
+      setTimeout(this.attackPlayer, 500);
+    },
+
+    healPlayer() {
+      this.roundNumber++;
+      const healValue = getRandomValue(5, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      setTimeout(this.attackPlayer, 500);
     },
   },
 });
