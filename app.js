e52648233f081949a228e5c019321c3a1496db5f
dejotb <dejotb@gmail.com>
2022-11-07 19:55:57 +0100

    added ending game functionality

--- app.js
+++ app.js
@@ -8,27 +8,55 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       roundNumber: 0,
+      result: null,
     };
+  },
+
+  watch: {
+    playerHealth(val) {
+      if (val <= 0 && this.monsterHealth <= 0) {
+        this.result = 'A draw!';
+      } else if (val <= 0) {
+        this.result = 'You lost!';
+      }
+    },
+
+    monsterHealth(val) {
+      if (val <= 0 && this.playerHealth <= 0) {
+        this.result = 'A draw!';
+      } else if (val <= 0) {
+        this.result = 'You Won!';
+      }
+    },
   },
 
   computed: {
     playerBarStyles() {
+      if (this.playerHealth <= 0) {
+        return { width: `0%` };
+      }
       return { width: `${this.playerHealth}%` };
     },
     monsterBarStyles() {
+      if (this.monsterHealth <= 0) {
+        return { width: `0%` };
+      }
       return { width: `${this.monsterHealth}%` };
     },
 
     activeSpecialAttack() {
       return this.roundNumber % 3 !== 0;
     },
-
-    // ableToHeal() {
-    //   return this.playerHealth >= 80;
-    // },
   },
 
   methods: {
+    restartGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.roundNumber = 0;
+      this.result = null;
+    },
+
     attackMonster() {
       this.roundNumber++;
       this.monsterHealth -= getRandomValue(5, 12);
@@ -55,6 +83,10 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       setTimeout(this.attackPlayer, 500);
+    },
+
+    surrender() {
+      this.result = 'You lost!';
     },
   },
 });
